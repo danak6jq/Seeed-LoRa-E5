38472e0608f9b916f30928a73fc67c8abf79f8aa
Dana <k6jq@comcast.net>
2021-12-20 09:47:58 -0800

    Configure project defaults enable low-power, disable SWD

--- Core/Inc/sys_conf.h
+++ Core/Inc/sys_conf.h
@@ -57,18 +57,18 @@ extern "C" {
 /**
   * @brief Enable MCU Debugger pins (dbg serial wires, sbg spi, etc)
   */
-#define DEBUGGER_ENABLED            1
+#define DEBUGGER_ENABLED            0
 
 /**
   * @brief Enable four wires usable as probes (two of them PROBE1 and PROBE2 used by the MW)
   */
-#define PROBE_PINS_ENABLED          1
+#define PROBE_PINS_ENABLED          0
 
 /**
   * @brief Disable Low Power mode
   * @note  0: LowPowerMode enabled. MCU enters stop2 mode, 1: LowPowerMode disabled. MCU enters sleep mode only
   */
-#define LOW_POWER_DISABLE           1
+#define LOW_POWER_DISABLE           0
 
 /* USER CODE BEGIN EC */
 
